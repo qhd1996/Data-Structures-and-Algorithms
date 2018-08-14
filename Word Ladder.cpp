8eb4bdd289267311647b26f8ed898c60e5275832
钱昊达 <qhd0081@vip.163.com>
2018-08-15 00:33:37 +0800

    Update Word Ladder.cpp

--- Word Ladder.cpp
+++ Word Ladder.cpp
@@ -4,13 +4,13 @@ Author: 錢昊達
 Date:2018-08-15，本来想08-14做完的，没来得及，唉 
 Description: leetcode上Word Ladder解答
 **************************************************/  
-/*
-这题非常符合BFS的结构，能想到用BFS做就不难，剩下的就是考察对STL的掌握程度了。
-做法是从beginword开始，访问它的邻居（只修改一个字母的单词），再访问它的邻居的邻居（当然必须是还没有被访问的），直到到达endword。为了方便，可以将endword加入到dict中，同时在申明一个变量dist，用来记录每次BFS之后的距离。
-leetcode网站提供的函数签名最后一个参数是vector类型的，处理起来非常不方便，所以我做的时候和leetcode上其他人一样使用set来做。
-注意这题可以作为set用法的一个练习。
-最后想说一句一定把STL常用数据结构熟练掌握吧。
-*/
+/**
+* 这题非常符合BFS的结构，能想到用BFS做就不难，剩下的就是考察对STL的掌握程度了。
+* 做法是从beginword开始，访问它的邻居（只修改一个字母的单词），再访问它的邻居的邻居（当然必须是还没有被访问的），直到到达endword。为了方便，可以将endword加入到dict中，同时在申明一个变量dist，用来记录每次BFS之后的距离。
+* leetcode网站提供的函数签名最后一个参数是vector类型的，处理起来非常不方便，所以我做的时候和leetcode上其他人一样使用set来做。
+* 注意这题可以作为set用法的一个练习。
+* 最后想说一句一定把STL常用数据结构熟练掌握吧。
+**/
 class Solution {
 public:
 	int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
