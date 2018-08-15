eba4d0da11b0564fc2bfdcb91d4b9d9de5008fd8
錢昊達 <qhd0081@vip.163.com>
2018-08-15 10:08:18 +0800

    Update Remove Duplicates from Sorted List II.cpp

--- Remove Duplicates from Sorted List II.cpp
+++ Remove Duplicates from Sorted List II.cpp
@@ -1,4 +1,9 @@
-
+/************************************************* 
+License: Apache 2.0
+Author: 錢昊達
+Date: 2018-08-15 
+Description: leetcode上Remove Duplicates from Sorted List II解答
+**************************************************/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
