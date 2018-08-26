ea0e077ed8c69d213d0830661012e6c0a21d8261
錢昊達 <qhd0081@vip.163.com>
2018-08-27 00:21:59 +0800

    Update Maximum Subarray.cpp

--- Dynamic Programming/Maximum Subarray.cpp
+++ Dynamic Programming/Maximum Subarray.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int size= nums.size();
+        int dp[size];
+        dp[0]=nums[0];
+        for(int i=1;i<size;i++){
+            dp[i]=max(nums[i],dp[i-1]+nums[i]);
+        }
+        int k=0;
+        for(int i=1;i<size;i++){
+            if(dp[i]>dp[k]){
+                k=i;
+            }
+        }
+        return dp[k];
+    }
+};
