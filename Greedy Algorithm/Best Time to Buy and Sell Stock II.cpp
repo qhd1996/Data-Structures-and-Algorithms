59c2dbc54dc0d2969cc9d7ed59c2167f60f943d6
錢昊達 <qhd0081@vip.163.com>
2018-08-27 23:02:14 +0800

    Update and rename Best Time to Buy and Sell Stock.cpp to Best Time to Buy and Sell Stock II.cpp

---
+++ Greedy Algorithm/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,12 @@
+//贪心法，低进高出，把所有整的价格差相加起来
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sum=0;
+        for(int i=1;i<prices.size();i++){
+            int diff=prices[i]-prices[i-1];
+            if(diff>0) sum+=diff;
+        }
+        return sum;
+    }
+};
