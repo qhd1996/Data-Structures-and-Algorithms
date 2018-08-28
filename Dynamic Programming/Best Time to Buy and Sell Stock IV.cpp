fcdeaebc3cc9bb741852c6044c97d219951fe065
錢昊達 <qhd0081@vip.163.com>
2018-08-28 14:01:59 +0800

    Update Best Time to Buy and Sell Stock IV.cpp

--- Dynamic Programming/Best Time to Buy and Sell Stock IV.cpp
+++ Dynamic Programming/Best Time to Buy and Sell Stock IV.cpp
@@ -1,29 +1,30 @@
 class Solution {
-    public:
-        int maxProfit(int k, vector<int> &prices) {
-            int len = prices.size();
-            if (len<2) return 0;
-            if (k>len/2){ // simple case
-                int ans = 0;
-                for (int i=1; i<len; ++i){
-                    ans += max(prices[i] - prices[i-1],0);
-                }
-             return ans;
-            }
-            int hold[k+1];
-            int rele[k+1];
-            for (int i=0;i<=k;++i){
-                hold[i] = INT_MIN;
-                rele[i] = 0;
-            }
-            int cur;
-            for (int i=0; i<len; ++i){
-                cur = prices[i];
-                for(int j=k; j>0; --j){
-                    rele[j] = max(rele[j],hold[j] + cur);
-                    hold[j] = max(hold[j],rele[j-1] - cur);
-                }
-            }
-            return rele[k];
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int maxProf=0;
+        
+        if(prices.size()<2)
+            return 0;
+        if(k>prices.size()/2){
+            for(int i=1; i<prices.size(); i++)
+                maxProf += max(prices[i]-prices[i-1], 0);
+            return maxProf;
         }
+        
+        int hold[k+1];
+        int rele[k+1];
+        for (int i=0;i<=k;++i){
+            hold[i] = INT_MAX;
+            rele[i] = 0;
+        }
+        
+        for(int i=0; i<prices.size(); i++){
+            for(int j=k; j>=1; j--){
+                rele[j] = max(rele[j], prices[i]-hold[j]);
+                hold[j] = min(hold[j], prices[i]-rele[j-1]);
+                maxProf=max(maxProf, rele[j]);
+            }
+        }
+        return maxProf;
+    }
 };
