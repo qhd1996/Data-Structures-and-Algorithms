7f2782db595b0fbe608147899d48a1e0a52556a9
錢昊達 <qhd0081@vip.163.com>
2018-10-31 15:28:31 +0800

    Create Climbing Stairs.cpp

---
+++ Dynamic Programming/Climbing Stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> res(n+1,0);
+        res[1]=1;
+        res[2]=2;
+        for(int i=3;i<=n;i++){
+            res[i]=res[i-1]+res[i-2];
+        }
+        return res[n];
+    }
+};
